5a0f618687c38f97b2362312a9ee49aa53caadc8
wiiziisco <wiizzardiisco@gmail.com>
2025-12-30 19:23:11 +0000

    Nettoyage fichiers inutiles (sw.js)

--- public/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Simple pass-through (nécessaire pour la PWA)
-  e.respondWith(fetch(e.request));
-});
