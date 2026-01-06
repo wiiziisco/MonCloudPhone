2537c1f665ed59b5828f77b5fc6d8eead157546d
wiiziisco <wiizzardiisco@gmail.com>
2026-01-06 19:39:51 +0000

    Fix: PWA Installation compliant (Icons + Manifest)

--- sw.js
+++ sw.js
@@ -1,13 +1,25 @@
-const CACHE_NAME = 'dodo-v1';
+const CACHE_NAME = 'dodo-v4';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://cdn-icons-png.flaticon.com/512/3004/3004797.png'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
