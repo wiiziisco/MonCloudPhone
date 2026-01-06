cce560ccecb843b3e4fb86a3f3a7e5f8f8e45841
wiiziisco <wiizzardiisco@gmail.com>
2026-01-06 17:24:12 +0000

    Ajout Service Worker sw.js complet

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'dodo-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
